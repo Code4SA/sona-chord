651593bac66810d0a136b12f006ac5e3f65922e9
JD Bothma <jbothma@gmail.com>
2016-02-17 16:51:26 +0200

    choose viz based on window width at load

--- embed.js
+++ embed.js
@@ -1,8 +1,13 @@
+if (window.innerWidth < 768) {
+    var heatSwitch = "heatmap/";
+} else {
+    var heatSwitch = "";
+}
 if (document.location.hostname == "localhost") {
-    var baseurl = "";
+    var baseurl = document.location.origin + "/";
 } else {
     var baseurl = "https://static.code4sa.org/sona-chord/";
 }
 
 document.write('<script type="text/javascript" src="' + baseurl + 'pym.js"></script>')
-document.write(" <script>var pymParent = new pym.Parent('tweetCircle', '" + baseurl + "index.html', {});</script>")
+document.write(" <script>var pymParent = new pym.Parent('tweetCircle', '" + baseurl + heatSwitch + "index.html', {});</script>")
